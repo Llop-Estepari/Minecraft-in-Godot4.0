b09c8d60fed98abbf6b4035c6735352cf5bab481
Llop <blackhear12@gmail.com>
2023-05-03 00:46:07 +0200

    Initial commit

---
+++ scenes/player.gd
@@ -0,0 +1,46 @@
+extends CharacterBody3D
+
+@onready var head = $Head
+
+const SPEED = 5.0
+const RUN_SPEED = 9.0
+const JUMP_VELOCITY = 14
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+
+var mouse_sens = 0.1
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= mouse_sens * event.relative.x
+		head.rotation_degrees.x -= mouse_sens * event.relative.y
+		head.rotation_degrees.x = clamp(head.rotation_degrees.x, -90, 90)
+
+
+func _physics_process(delta):
+	if Input.is_action_just_pressed("action_esc"): get_tree().quit()
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("action_jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
